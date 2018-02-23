8c0d79d2e2bce40441be57bdb8498e7e3183a897
Tim Windelschmidt <fionera@fionera.de>
2018-02-23 11:40:47 +0100

    Added JWT Authentication Bundle

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,6 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Oneup\FlysystemBundle\OneupFlysystemBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
